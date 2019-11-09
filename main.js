0235acd369df0256edb0ab7eff2c0425a4c7c994
Annie Huang <annie.hn@gmail.com>
2019-11-09 21:53:50 +1100

    Option2: use jQuery animation. Scroll works when clicking the link, on all browsers. But scroll up and down with mouse or drap up and down will not work.

---
+++ main.js
@@ -0,0 +1,20 @@
+// Option 2 - jQuery Smooth Scroll
+
+$('.navbar a').on('click', function (e) {
+    // console.log(this.hash);
+    // You will see what link is being clicked.
+    // Print
+    // #home
+    // #about
+    // #service
+    // #contact
+
+    if(this.hash !== '') {
+        e.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    }
+});
